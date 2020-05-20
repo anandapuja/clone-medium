97ee2aa4b6eb0a2980eed932b265c76e3f2c448b
verypei <verrymariyanto@gmail.com>
2020-05-20 22:58:29 +0700

    bookmark

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,24 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
 
 
 
 export default function Card(props){//props bisa mndpt data dari parent yaitu App.js
-    
+    const [bookmark,setBookmark] = useState([])
     useEffect(()=>{
         let url = `https://localhost:3000/articles/me/bookmarked`
         fetch(url)
         .then(resp=>resp.json())
         .then(resp=>{
-            
+            setBookmark(resp.data)
         })
         .catch(err=>{
             console.log(err)
         })
     })
 
-    function deleteBookmark(){
-        console.log("entering delete bookmarked!!!!!!!!!!!!!")
+    function deleteBookmark(id){
+        // console.log("entering delete bookmarked!!!!!!!!!!!!!")
+        let newBookmark = []
+        for(let i=0;i<bookmark.length;i++){
+            if(bookmark[i].id!==id){
+                newBookmark.push(bookmark[i])
+            }
+        }
+        setBookmark(newBookmark)
     }
 
     return(
@@ -34,7 +41,7 @@ export default function Card(props){//props bisa mndpt data dari parent yaitu Ap
                         </p>
                 </div>
                 <div className="options">
-                    <button onClick={deleteBookmark} className="deleteButton"> <FaTrashAlt/>delete</button>
+                    <button onClick={()=>{deleteBookmark()}} className="deleteButton"><FaTrashAlt/>delete</button>
                 </div>
             </div>
         </div>
